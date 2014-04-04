3b29a8a526cbbbbdd6a2383cdadb975ebce720d5
Ariel <ariel.lopezf@gmail.com>
2014-04-04 11:28:32 +0800

    programitacally create radio button

--- src/com/example/radiobutontutorial/MainActivity.java
+++ src/com/example/radiobutontutorial/MainActivity.java
@@ -2,28 +2,36 @@ package com.example.radiobutontutorial;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
 	RadioGroup rg;
+	Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1); 
-		  final String value = ((RadioButton)findViewById(rg.getCheckedRadioButtonId() )).getText().toString(); 
+		setContentView(R.layout.activity_main); 
+		RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);  
+	 
+		for(int i=0; i<6; i++)
+		{
+			  RadioButton rb = new RadioButton(this);
+		       rb.setText("rdo"+i);
+		       rb.setId(i);
+		       rg.addView(rb);           
+		  }      
+		 
+	
 		 rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){  
 			@Override
 			public void onCheckedChanged(RadioGroup group, int checkedId) {
-				 ShowDialog(value);		
-				
+				   final String value = ((RadioButton)findViewById(checkedId)).getText().toString(); 
+				 ShowDialog(value);		 
 			}
 		});
 	}
@@ -38,8 +46,7 @@ public class MainActivity extends Activity {
 		   public void onClick(DialogInterface dialog, int which) {
 		      // TODO Add your code for the button here.
 		   }
-		});
-		// Set the Icon for the Dialog 
+		}); 
 		alertDialog.show();
 	}
 	
